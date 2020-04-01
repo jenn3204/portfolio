6310d7177f6f066a08697cac7bea612bc11a5021
jenniferjaque <jenn3204@stud.kea.dk>
2020-04-01 23:59:42 +0200

    end af day 1

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener("DOMContentLoaded", start);
 function start() {
   console.log("start");
   document.querySelector("#scroll_container").addEventListener("scroll", scrolling);
+  document.querySelector("#svg_blue").addEventListener("mousemove", moveMouse);
 }
 
 function scrolling() {
@@ -12,4 +13,11 @@ function scrolling() {
   let scrollContainer = document.querySelector("#scroll_container");
   let ratio = scrollContainer.scrollTop / (scrollContainer.scrollHeight - scrollContainer.clientHeight);
   document.querySelector("#scroll_bar").style.left = ratio * 98 + "vw";
+}
+
+function moveMouse(e) {
+  console.log("her");
+  document.querySelector("#clipCircle").setAttribute("cx", (e.pageX / window.innerWidth) * 1920);
+  document.querySelector("#clipCircle").setAttribute("cy", (e.pageY / window.innerHeight) * 1920 * (window.innerHeight / window.innerWidth));
+  e.preventDefault();
 }
