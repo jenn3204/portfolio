2358bc27ef4d28017070a88109cc85ae066bf0d1
jenniferjaque <jenn3204@stud.kea.dk>
2020-04-03 00:43:36 +0200

    works done (section 3)

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ function start() {
   document.querySelector("#svg_blue").addEventListener("mousemove", moveMouse);
 
   getSvg("svg/shape.svg", placeShape1);
+  getSvg("svg/shape.svg", placeShape3);
   getJson("data.json");
 
   if (window.innerWidth < "700") {
     document.querySelector("#clipCircle").style.r = "2000";
-    document.querySelector("#section1").style.height = "100vh";
+    document.querySelector("#section1").style.height = "70vh";
   }
 
   document.querySelector("#paaske").addEventListener("click", showPaaske);
@@ -35,6 +36,12 @@ async function getSvg(file, callback) {
 
 function placeShape1(svg) {
   document.querySelector("#shape1").innerHTML = svg;
+  document.querySelector("#shape1 svg g path").classList.add("bubbly");
+}
+
+function placeShape3(svg) {
+  document.querySelector("#shape3").innerHTML = svg;
+  document.querySelector("#shape3 svg g path").classList.add("bubbly");
 }
 
 function scrolling() {
@@ -57,28 +64,28 @@ function moveMouse(e) {
 }
 
 function showPaaske() {
-  console.log("paaske");
   document.querySelector("#works_info").scrollIntoView();
   document.querySelector("#works_info h3").textContent = jsonData[0].title;
   document.querySelector("#works_info p").textContent = jsonData[0].describtion;
   document.querySelector("#works_info #knap").textContent = jsonData[0].knap;
   document.querySelector("#works_info #knap").href = jsonData[0].link;
+  document.querySelector("#works_video").src = jsonData[0].movie;
 }
 
 function showPort() {
-  console.log("paaske");
   document.querySelector("#works_info").scrollIntoView();
   document.querySelector("#works_info h3").textContent = jsonData[1].title;
   document.querySelector("#works_info p").textContent = jsonData[1].describtion;
   document.querySelector("#works_info #knap").textContent = jsonData[1].knap;
   document.querySelector("#works_info #knap").href = jsonData[1].link;
+  document.querySelector("#works_video").src = jsonData[1].movie;
 }
 
 function showMuseum() {
-  console.log("paaske");
   document.querySelector("#works_info").scrollIntoView();
   document.querySelector("#works_info h3").textContent = jsonData[2].title;
   document.querySelector("#works_info p").textContent = jsonData[2].describtion;
   document.querySelector("#works_info #knap").textContent = jsonData[2].knap;
   document.querySelector("#works_info #knap").href = jsonData[2].link;
+  document.querySelector("#works_video").src = jsonData[2].movie;
 }
