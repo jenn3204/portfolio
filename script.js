9ffab5e8c16237ea29e49c5f053ee8cbb3ac9d57
jenniferjaque <jenn3204@stud.kea.dk>
2020-04-08 22:58:39 +0200

    working on section 2

--- script.js
+++ script.js
@@ -3,18 +3,26 @@
 window.addEventListener("DOMContentLoaded", start);
 
 let jsonData = [];
+let splashClicked = false;
 
 function start() {
   console.log("start");
   document.querySelector("#scroll_container").addEventListener("scroll", scrolling);
   document.querySelector("#svg_blue").addEventListener("mousemove", moveMouse);
 
-  document.querySelector("#section1").addEventListener("click", function() {
-    document.querySelector("#clipCircle").style.r = "2000";
+  document.querySelector("#section1").addEventListener("click", function () {
+    if (splashClicked == false) {
+      splashClicked = true;
+      document.querySelector("#clipCircle").style.r = "2000";
+    } else if (splashClicked == true) {
+      splashClicked = false;
+      document.querySelector("#clipCircle").style.r = "200";
+    }
   });
 
-  getSvg("svg/shape.svg", placeShape1);
-  getSvg("svg/shape.svg", placeShape3);
+  getSvg("svg/shape.svg", placeShapes);
+  getSvg("svg/skills.svg", placeSkills);
+  getSvg("svg/arrow.svg", placeArrows);
   getJson("data.json");
 
   if (window.innerWidth < "700") {
@@ -22,14 +30,18 @@ function start() {
     document.querySelector("#section1").style.height = "70vh";
   }
 
+  document.querySelector("#works_info").classList.add("hide");
   document.querySelector("#paaske").addEventListener("click", showPaaske);
   document.querySelector("#port").addEventListener("click", showPort);
   document.querySelector("#museum").addEventListener("click", showMuseum);
+  document.querySelector("#close").addEventListener("click", closeWork);
 }
 
 async function getJson(file) {
   let response = await fetch(file);
   jsonData = await response.json();
+
+  document.querySelector("#section2_about p").textContent = jsonData[3].describtion;
 }
 
 async function getSvg(file, callback) {
@@ -38,14 +50,22 @@ async function getSvg(file, callback) {
   callback(svg);
 }
 
-function placeShape1(svg) {
+function placeShapes(svg) {
   document.querySelector("#shape1").innerHTML = svg;
+  document.querySelector("#shape2").innerHTML = svg;
+  document.querySelector("#shape3").innerHTML = svg;
+
   document.querySelector("#shape1 svg g path").classList.add("bubbly");
+  document.querySelector("#shape2 svg g path").classList.add("bubbly");
+  document.querySelector("#shape3 svg g path").classList.add("bubbly");
 }
 
-function placeShape3(svg) {
-  document.querySelector("#shape3").innerHTML = svg;
-  document.querySelector("#shape3 svg g path").classList.add("bubbly");
+function placeSkills(svg) {
+  document.querySelector("#skills_svg").innerHTML = svg;
+}
+
+function placeArrows(svg) {
+  document.querySelector("#arrow1").innerHTML = svg;
 }
 
 function scrolling() {
@@ -64,7 +84,13 @@ function moveMouse(e) {
 }
 
 function showPaaske() {
-  document.querySelector("#works_info").scrollIntoView();
+  document.querySelector("#works_info").classList = "";
+  document.querySelector("#works_info").classList.add("showopacity");
+
+  if (window.innerWidth < "700") {
+    document.querySelector("#works_info").scrollIntoView();
+    console.log(window.innerWidth);
+  }
   document.querySelector("#works_info h3").textContent = jsonData[0].title;
   document.querySelector("#works_info p").textContent = jsonData[0].describtion;
   document.querySelector("#works_info #knap").textContent = jsonData[0].knap;
@@ -73,7 +99,12 @@ function showPaaske() {
 }
 
 function showPort() {
-  document.querySelector("#works_info").scrollIntoView();
+  document.querySelector("#works_info").classList = "";
+  document.querySelector("#works_info").classList.add("showopacity");
+
+  if (window.innerWidth < "700") {
+    document.querySelector("#works_info").scrollIntoView();
+  }
   document.querySelector("#works_info h3").textContent = jsonData[1].title;
   document.querySelector("#works_info p").textContent = jsonData[1].describtion;
   document.querySelector("#works_info #knap").textContent = jsonData[1].knap;
@@ -82,10 +113,21 @@ function showPort() {
 }
 
 function showMuseum() {
-  document.querySelector("#works_info").scrollIntoView();
+  document.querySelector("#works_info").classList.add("hide");
+  document.querySelector("#works_info").classList = "";
+  document.querySelector("#works_info").classList.add("showopacity");
+
+  if (window.innerWidth < "700") {
+    document.querySelector("#works_info").scrollIntoView();
+  }
   document.querySelector("#works_info h3").textContent = jsonData[2].title;
   document.querySelector("#works_info p").textContent = jsonData[2].describtion;
   document.querySelector("#works_info #knap").textContent = jsonData[2].knap;
   document.querySelector("#works_info #knap").href = jsonData[2].link;
   document.querySelector("#works_video").src = jsonData[2].movie;
+}
+
+function closeWork() {
+  document.querySelector("#works_info").classList = "";
+  document.querySelector("#works_info").classList.add("hideopacity");
 }
